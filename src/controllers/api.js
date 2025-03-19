56e42a55f7d319e41938d673e5b41f38f2a6b1d7
vishallmaurya <vishallmaurya210@gmail.com>
2025-03-19 09:56:11 +0530

    api controller get updated

---
+++ src/controllers/api.js
@@ -0,0 +1,34 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/users.js";
+
+
+const predictTweetSentiment = asyncHandler(async (req, res) => {
+    try {
+        const { tweet } = req.body;
+        
+        if(tweet?.trim() === "") {
+            throw new ApiError(400, "Fields are mandatory");
+        }
+
+        const url = process.env.SENTIMENT_API_URL + "/" + process.env.SENTIEMTN_ENDPOINT;
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ tweet }),
+        });
+
+        const data = await response.json();
+        
+        return res.status(200).
+            json(new ApiResponse(200, data, "Get the data successfully"));
+    } catch (error) {
+        throw new ApiError(400, error?.message || "Error occur during getting output");
+    }
+})
+
+export { predictTweetSentiment };
